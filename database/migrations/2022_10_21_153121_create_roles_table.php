27459cff04e798c64e86876553ebd108327d073a
tiana <tianacoker1@gmail.com>
2022-10-21 17:05:05 +0100

    create database:migrations

--- database/migrations/2022_10_21_153121_create_roles_table.php
+++ database/migrations/2022_10_21_153121_create_roles_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id('role_id');
             $table->string('name');
-            $table->integer('role')->default(4);
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
